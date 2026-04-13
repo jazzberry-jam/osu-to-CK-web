6aabc1b11ca01a35543ee1edb62ec2039ea7a734
Michael Nieto <michaeln.gamedev@gmail.com>
2026-04-13 13:19:11 -0700

    Update CKConverter.js

--- Scripts/CKConverter.js
+++ Scripts/CKConverter.js
@@ -2,112 +2,133 @@ var parser = module.require("OsuParser");
 var toOutput = "";
 
 // osu!mania tap and hold notes have x coordinate ranges that map to the different lanes (6 in our case)
-// Deleted: FNF handling for 1p and 2p 
+// Deleted: FNF handling for 1p and 2p
 function hitobj_x_to_track_number(hitobj_x) {
-	var track_number = 0;
+  var track_number = 0;
 
-	if (hitobj_x <= 32) {
-		track_number = 0;
-	} else if (hitobj_x <= 96) {
-		track_number = 1;
-	} else if (hitobj_x <= 160) {
-		track_number = 2;
-	} else if (hitobj_x <= 224) {
-		track_number = 3;
-	} else if (hitobj_x <= 352) {
-		track_number = 4;
-	} else if (hitobj_x <= 416) {
-		track_number = 5;
-	} else {
-		track_number = 6;
-	}
-	
-	return track_number;
+  if (hitobj_x <= 32) {
+    track_number = 0;
+  } else if (hitobj_x <= 96) {
+    track_number = 1;
+  } else if (hitobj_x <= 160) {
+    track_number = 2;
+  } else if (hitobj_x <= 224) {
+    track_number = 3;
+  } else if (hitobj_x <= 352) {
+    track_number = 4;
+  } else if (hitobj_x <= 416) {
+    track_number = 5;
+  } else {
+    track_number = 6;
+  }
+
+  return track_number;
 }
 
 function msToTime(s) {
-	var ms = s % 1000;
-	s = (s - ms) / 1000;
-	var secs = s % 60;
-	s = (s - secs) / 60;
-	var mins = s % 60;
-	var hrs = (s - mins) / 60;
-	return hrs + ':' + mins + ':' + secs + '.' + ms;
+  var ms = s % 1000;
+  s = (s - ms) / 1000;
+  var secs = s % 60;
+  s = (s - secs) / 60;
+  var mins = s % 60;
+  var hrs = (s - mins) / 60;
+  return hrs + ":" + mins + ":" + secs + "." + ms;
 }
 
 function append_to_output(str) {
-		toOutput += (str)
+  toOutput += str;
 }
 
-module.export("CKConverter", function(osu_file_contents, options) {
-	var beatmap = parser.parseContent(osu_file_contents)
+module.export("CKConverter", function (osu_file_contents, options) {
+  var beatmap = parser.parseContent(osu_file_contents);
 
-	console.log(beatmap);
-	console.log(options);
+  console.log(beatmap);
+  console.log(options);
 
-	if (options.song === "") {
-		if (beatmap.Title !== "" || beatmap.Title !== undefined) {
-			options.song = beatmap.Title;
-		} 
-		if (options.song === undefined) {
-			options.song = "songName";
-		}
-	}
-	if (options.bpm === "") {
-		options.bpm = beatmap.bpmMax;
-	}
-	
-	options.bpm = parseInt(options.bpm);
-	
-	if (isNaN(options.bpm)) {
-		options.bpm = 120;
-	}
-	
-	if (options.time_signature === "") {
-		options.time_signature = {"numerator":beatmap.timingPoints[0].timingSignature, "denominator":4};
-	}
-	else {
-		var time_sig_split = options.time_signature.split("/")
-		options.time_signature = {"numerator":time_sig_split[0], "denominator":time_sig_split[1]};
-	}
+  if (options.song === "") {
+    if (beatmap.Title !== "" || beatmap.Title !== undefined) {
+      options.song = beatmap.Title;
+    }
+    if (options.song === undefined) {
+      options.song = "songName";
+    }
+  }
+  if (options.bpm === "") {
+    options.bpm = beatmap.bpmMax;
+  }
 
-	var beatUnitInMs = ((60 / options.bpm) * 1000); // beat unit length in ms (denominator)
-	
-	append_to_output(`{\n\t"songId": "${options.song}",`)
-	append_to_output(`\n\t"bpm": ${options.bpm},`);
-	append_to_output(`\n\t"timeSignature": {\n\t\t"beatsPerBar": ${options.time_signature.numerator},\n\t\t"beatUnit": ${options.time_signature.denominator}\n\t},`)
-	// append_to_output(`\n\t"resolution": 16,`)
-	append_to_output(`\n\t"notes": [`)
-	
-	// Handling of per-note output
-	for (var i = 0; i < beatmap.hitObjects.length; i++) {
-		if (i == 0) { append_to_output(`\n\t\t{`); }
-		else 		{ append_to_output(`,\n\t\t{`); }
-		
-		var currentObj = beatmap.hitObjects[i];
-		var beat = Math.round((currentObj.startTime / beatUnitInMs) * 100) / 100;
-		var lane = hitobj_x_to_track_number(currentObj.position.get("x"));
-		var noteType = "";
-		var duration = "";
-		
-		if (currentObj.objectName == "circle") {
-			noteType = "Tap";
-		}
-		else {
-			noteType = "Hold";
-		}
-		
-		append_to_output(`\n\t\t\t"beat": ${beat},\n\t\t\t"lane": ${lane},\n\t\t\t"noteType": "${noteType}"`);
-		
-		if (noteType == "Hold") {
-			duration = Math.round(((currentObj.endTime / beatUnitInMs) - beat ) * 100) / 100;
-			append_to_output(`,\n\t\t\t"duration": ${duration}`);
-		}
-		
-		append_to_output(`\n\t\t}`);
-	}
-	
-	append_to_output(`\n\t]\n}`);
+  options.bpm = parseInt(options.bpm);
 
-	return toOutput
-})
+  if (isNaN(options.bpm)) {
+    options.bpm = 120;
+  }
+
+  if (options.time_signature === "") {
+    options.time_signature = {
+      numerator: beatmap.timingPoints[0].timingSignature,
+      denominator: 4,
+    };
+  } else {
+    var time_sig_split = options.time_signature.split("/");
+    options.time_signature = {
+      numerator: time_sig_split[0],
+      denominator: time_sig_split[1],
+    };
+  }
+
+  var beatUnitInMs = (60 / options.bpm) * 1000; // beat unit length in ms (denominator)
+
+  append_to_output(`{\n\t"songId": "${options.song}",`);
+  append_to_output(`\n\t"bpm": ${options.bpm},`);
+  append_to_output(
+    `\n\t"timeSignature": {\n\t\t"beatsPerBar": ${options.time_signature.numerator},\n\t\t"beatUnit": ${options.time_signature.denominator}\n\t},`,
+  );
+  // append_to_output(`\n\t"resolution": 16,`)
+  append_to_output(`\n\t"notes": [`);
+
+  // Handling of per-note output
+  for (var i = 0; i < beatmap.hitObjects.length; i++) {
+    if (i == 0) {
+      append_to_output(`\n\t\t{`);
+    } else {
+      append_to_output(`,\n\t\t{`);
+    }
+
+    var currentObj = beatmap.hitObjects[i];
+
+    var timeSigNumerator = options.time_signature.split("/")[0];
+    var absoluteBeat =
+      Math.round((currentObj.startTime / beatUnitInMs) * 100) / 100;
+    var beat = Math.floor(absoluteBeat % timeSigNumerator);
+    var measure = Math.floor(absoluteBeat / timeSigNumerator);
+    //var beat = Math.round((currentObj.startTime / beatUnitInMs) * 100) / 100;
+
+    var absoluteLane = hitobj_x_to_track_number(currentObj.position.get("x"));
+    var lane = ((absoluteLane - 1) % 3) + 1; // Wrap lane value if it exceeds 3
+    var trackState = absoluteLane > 3 ? 1 : 0; // Make state 1 if it had to wrap its value
+    var noteType = "";
+    var duration = "";
+
+    if (currentObj.objectName == "circle") {
+      noteType = "Tap";
+    } else {
+      noteType = "Hold";
+    }
+
+    append_to_output(
+      `\n\t\t\t"beat": ${beat},\n\t\t\t"measure": ${measure},\n\t\t\t"lane": ${lane},\n\t\t\t"trackState": ${trackState},\n\t\t\t"noteType": "${noteType}"`,
+    );
+
+    if (noteType == "Hold") {
+      duration =
+        Math.round((currentObj.endTime / beatUnitInMs - beat) * 100) / 100;
+      append_to_output(`,\n\t\t\t"duration": ${duration}`);
+    }
+
+    append_to_output(`\n\t\t}`);
+  }
+
+  append_to_output(`\n\t]\n}`);
+
+  return toOutput;
+});
